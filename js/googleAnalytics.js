cc221de7e74b4f12075891150fe5cad51ccf2a7b
thedanindanger <dan@puttingthedanindanger.com>
2017-07-26 15:56:42 -0500

    inital commit

---
+++ js/googleAnalytics.js
@@ -0,0 +1,34 @@
+﻿
+//functions to insert google id, user id, and create new sessions
+
+//set new google account, optional name assignment
+
+setAccountNum = function() {
+    var n = document.getElementsByName("newAccountNum")[0].value;
+    ga('create', n, 'auto');
+    //overwrites tracker number for 'ga' objects
+}
+
+sendPageView = function() {
+    var url = document.getElementsByName("newUrl")[0].value;
+    ga('send', 'pageview',
+        { page: url });
+}
+
+
+
+setUserIDNum = function() {
+    var url = document.getElementsByName("newUserId")[0].value;
+    ga('set', 'userId', id);
+}
+
+sendNewSessionEvent = function() {
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'newSession',
+        eventAction: 'start',
+        eventLabel: 'trigger',
+        sessionControl: 'start')
+}
+
+    
